42c01df5aa1cf6c578e27c0fe104c8fb7516fb58
David Pina <davidpina14@gmail.com>
2017-05-19 16:59:32 -0500

    First commit

---
+++ app/controllers/novels_controller.rb
@@ -0,0 +1,49 @@
+class NovelsController < ApplicationController
+  def index
+    if session[:id].nil?
+      redirect_to '/users'
+    else
+      @user = session[:name]
+      @books = Novel.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @authors = Author.all
+  end
+
+  def create
+    @auth = Author.new(name:params[:author_name])
+
+    if @auth.valid?
+      @auth.save
+      @book = Novel.new(title:params[:novel_title],author:Author.find(@auth.id))
+      if @book.valid?
+        @book.save
+        @review = Review.new(title:params[:review_title],user:User.find(session[:id]),book:Book.find(@book.id))
+        if @review.valid?
+          @review.save
+          redirect_to '/'
+        end
+      end
+    else
+      # flash[:errors]= ["ERROR:404"]
+      redirect_to '/novels/new'
+    end
+  end
+
+  # private 
+  #   def novel_params
+  #     params.require(:novel).permit(:title,:author)
+  #   end
+  #   def author_params
+  #     params.require(:author).permit(:name)
+  #   end
+  #   def review_params
+  #     params.require(:review).permit(:title, :rating)
+  #   end
+
+end
---
+++ db/migrate/20170519161807_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.references :user, index: true, foreign_key: true
+      t.references :book, index: true, foreign_key: true
+      t.integer :rating
+
+      t.timestamps null: false
+    end
+  end
+end
